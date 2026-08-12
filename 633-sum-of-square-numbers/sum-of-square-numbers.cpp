cdd593aad9275c06f1ea22898435f8da3dc04626
ritishchandra23 <ritishchandra23@gmail.com>
2026-08-12 23:33:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.92%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,13 @@
+#include<cmath>
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(long a = 0; a * a <= c; a++) {
+            double b = sqrt(c - (a * a));
+            if(b == (int)b) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
